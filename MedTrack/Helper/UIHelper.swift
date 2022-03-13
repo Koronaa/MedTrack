f32b511ec2d92951f83ac3750cf43362b1fcdef0
Koronä <konara.sajith@gmail.com>
2022-03-13 10:26:29 +0530

    Implements BRYXBanner

--- MedTrack/Helper/UIHelper.swift
+++ MedTrack/Helper/UIHelper.swift
@@ -7,6 +7,13 @@
 
 import Foundation
 import UIKit
+import BRYXBanner
+
+enum BannerType{
+    case Error
+    case Warning
+    case Success
+}
 
 class UIHelper{
     
@@ -76,6 +83,30 @@ class UIHelper{
         }
     }
     
+    private static func makeBanner(title:String,message:String,style:BannerType) -> Banner{
+        var bannerBackground:UIColor!
+        switch style {
+        case .Error:
+            bannerBackground = .MedTrackErrorRed
+        case .Success:
+            bannerBackground =  .MedTrackSuccessGreen
+        case .Warning:
+            bannerBackground =  .MedTrackWarningYellow
+        }
+        
+        let banner = Banner(title: title, subtitle: message, backgroundColor: bannerBackground)
+        banner.textColor = .black
+        banner.titleLabel.font = UIFont(name: "Avenir-Heavy", size: 16)
+        banner.detailLabel.font = UIFont(name: "Avenir-Medium", size: 15)
+        return banner
+    }
     
+    static func showUserMessage(for message:UserMessage,type:BannerType = .Error){
+        DispatchQueue.main.async {
+            let banner = makeBanner(title: message.title, message: message.message, style: type)
+            banner.dismissesOnTap = true
+            banner.show(duration: 2.0)
+        }
+    }
     
 }
